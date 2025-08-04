c2bc4f2bedf10cbbf48fc97941935c7f240432c0
traxxoo <thecodealchemy@gmail.com>
2025-08-04 14:35:33 +0530

    done with the project

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from stats import return_count
 from stats import letter_count
+from stats import listing_the_dict
+import sys 
+
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -7,12 +10,26 @@ def get_book_text(filepath):
         return file_conents
 
 def main():
-    file_material = get_book_text("books/frankenstein.txt")
-    word_count = return_count(file_material)
-    print(f"{word_count} words found in the document")
-    each_letter_count = letter_count(file_material)
-    print(each_letter_count)
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
 
+    file_material = get_book_text(sys.argv[1])
+
+    word_count = return_count(file_material)
+    each_letter_count = letter_count(file_material)
+    
+    char_list = listing_the_dict(each_letter_count)
+
+    print(f"============ BOOKBOT ============")
+    print(f"Analyzing book found at {sys.argv[1]}...")
+    print(f"----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+
+    for data in char_list:
+        if data["char"].isalpha() :
+            print(f"{data["char"]}: {data["num"]}")
 
 
 main()
--- stats.py
+++ stats.py
@@ -15,3 +15,18 @@ def letter_count(file_text):
             each_letter_count[letter]= 1
     
     return each_letter_count
+
+def listing_the_dict(letter_data):
+    character_metadata = []
+    for key,value in letter_data.items():
+        meta_data_dict = {}
+        meta_data_dict["char"] = key
+        meta_data_dict["num"] = value
+        character_metadata.append(meta_data_dict)
+    character_metadata.sort(reverse = True, key = sort_on )
+    return character_metadata
+
+
+
+def sort_on(items):
+    return items["num"]
